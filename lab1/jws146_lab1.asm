d26714ddf3f9022e6aae094f02a81338baf44798
jwSharp <jws146@pitt.edu>
2022-09-18 12:41:19 -0400

    Read input of integers and floats.
    Set v0 to 5 and 6 to prompt for user input of integers and floats, respectively. Print the results.
    
    When prompting for floats, f0 was interpreted as an integer. Lines were commented out.

--- lab1/jws146_lab1.asm
+++ lab1/jws146_lab1.asm
@@ -1,16 +1,18 @@
 # Jacob Sharp jws146
 
 .global main
+
 main:
-	# load immediates into registers
-	li t0, 1
-	li t1, 2
-	li t2, 3
-	
-	# move values between registers
-	move a0, t0 # a0 = t0
-	move v0, t1 # v0 = t1
-	move t2, zero # t2 = 0
+	registers:
+		# load immediates into registers
+		li t0, 1
+		li t1, 2
+		li t2, 3
+		
+		# move values between registers
+		move a0, t0 # a0 = t0
+		move v0, t1 # v0 = t1
+		move t2, zero # t2 = 0
 	
 	explore:
 		# hexidecimal with li
@@ -22,17 +24,54 @@ main:
 		# zero register cannot be changed
 		li zero, 10
 
-	# print(123)
-	li a0, 123
-	li v0, 1
-	syscall
+	output:
+		# print(123)
+		li a0, 123
+		li v0, 1
+		syscall
+		
+		# print(\n)
+		li a0, '\n'
+		li v0, 11
+		syscall
+		
+		# print(456)
+		li a0, 456
+		li v0, 1
+		syscall
 	
-	# print(\n)
-	li a0, '\n'
-	li v0, 11
-	syscall
-	
-	# print(456)
-	li a0, 456
-	li v0, 1
-	syscall
+	input:
+		## integer
+		
+		# print(\n)
+		li a0, '\n'
+		li v0, 11
+		syscall
+		
+		# v0 = input(int( ))
+		li v0, 5
+		syscall
+		
+		# print( v0 )
+		move a0, v0
+		li v0, 1
+		syscall
+		
+		
+		## float
+		
+		# print(\n)
+		#li a0, '\n'
+		#li v0, 11
+		#syscall
+		
+		# f0 = input(float( ))
+		#li v0, 6
+		#syscall
+		
+		# print( f0 )
+		#move f12, f0
+		#li v0, 2
+		#syscall
+		
+		
