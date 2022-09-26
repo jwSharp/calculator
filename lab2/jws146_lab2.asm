c4bd245ec872df0826d4e8fd9beb348d31591154
jwSharp <jws146@pitt.edu>
2022-09-26 15:03:02 -0400

    Create lab2. Add macro for printing to std output.

---
+++ lab2/jws146_lab2.asm
@@ -0,0 +1,20 @@
+# Jacob Sharp
+# JWS146
+
+# preserves a0, v0
+.macro print_str %str
+	.data
+	print_str_message: .asciiz %str
+	.text
+	push a0
+	push v0
+	la a0, print_str_message
+	li v0, 4
+	syscall
+	pop v0
+	pop a0
+.end_macro
+
+.globl main
+main:
+	
