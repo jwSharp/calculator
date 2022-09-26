086a36ed65aace2908cd78266ee4fbb877594197
jwSharp <jws146@pitt.edu>
2022-09-26 16:08:01 -0400

    Write macro for special characters. Does not work.

--- lab2/jws146_lab2.asm
+++ lab2/jws146_lab2.asm
@@ -15,6 +15,53 @@
 	pop a0
 .end_macro
 
+.macro print_special %str
+# prints a special character
+	.data
+	character: %str
+	newline: .asciiz "newline"
+	tab: .asciiz "tab"
+	.text
+	push a0
+	push a1
+	push v0
+	
+	# switch(character):
+	_switch_character:
+		la a0, character
+		
+		# if(character == "newline"):
+		la a1, newline
+		beq a0, a1, _case_newline
+		
+		# elif(character == "tab"):
+		la a1, newline
+		beq a0, a1, _case_tab
+		
+		# else: # invalid character
+		j _default
+		
+		_case_newline:
+			li a0, '\n'
+			li v0, 11
+			syscall
+			j _break
+			
+		_case_tab:
+			li a0, '\t'
+			li v0, 11
+			syscall
+			j _break
+		
+		_default:
+			print_str "Invalid character"
+		_break:
+	
+	pop a0
+	pop a1
+	pop v0
+.end_macro
+
 .data
 	display: .word 0
 	operation: .word 0
@@ -22,7 +69,22 @@
 
 .globl main
 main:
+	# print( "Hello. Welcome!" )
+	print_str "Hello. Welcome!"
 	
+	# while(True) {
+_loop:
+	# print(display)
+	lw a0, display
+	li v0, 1
+	syscall
+	
+	# print("Operation (=,+,-,*,/,c,q):")
+	print_str "Operation (=, +, -, *, /, c, q:"
+	
+	
+	# }
+	#j _loop
 	
 	
 	# end program
